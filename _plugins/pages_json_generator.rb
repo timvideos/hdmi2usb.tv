d534c2852b07cab0b130bdaf5fd478c6a4f01938
Tim 'mithro' Ansell <mithro@mithis.com>
2015-08-02 13:43:54 +1000

    Mkdir the first time.

--- _plugins/pages_json_generator.rb
+++ _plugins/pages_json_generator.rb
@@ -32,6 +32,7 @@ module Jekyll
       site.pages.each { |page| pages['pages'] << { 'basename' => page.basename, 'data' => page.data, 'url' => page.url } }
       site.posts.each { |post| pages['posts'] << { 'slug'     => post.slug,     'data' => post.data, 'url' => post.url } }
 
+      Dir::mkdir(site.dest) if !File.directory? site.dest
       pages_path = File.join(site.dest, 'pages.json')
       File.open(pages_path, 'w') do |f|
         f.write(pages.to_json)
